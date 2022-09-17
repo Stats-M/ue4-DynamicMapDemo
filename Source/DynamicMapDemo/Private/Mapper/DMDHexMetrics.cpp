aec2b087a8266f7cac7e4c5d684678159e1ec5c0
Станислав М <deafstats@live.ru>
2022-09-17 22:41:31 +0300

    Updates

--- Source/DynamicMapDemo/Private/Mapper/DMDHexMetrics.cpp
+++ Source/DynamicMapDemo/Private/Mapper/DMDHexMetrics.cpp
@@ -1,14 +1,36 @@
 // DynamicMap Demo. Copyright (C) 2022 Stanislav Moskalev
 
-
 #include "Mapper/DMDHexMetrics.h"
 
-UDMDHexMetrics::UDMDHexMetrics()
+/**
+ * Static members initialization 
+ * (outside of any class definitions).
+ */
+const float UDMDHexMetrics::OuterRadius = 10.0f;
+
+const float UDMDHexMetrics::InnerRadius = UDMDHexMetrics::OuterRadius * 0.866025404f;
+
+const TArray<FVector> UDMDHexMetrics::HexMetrics3D =
 {
-    DefaultHexVertices.Corner1 = FVector(0.0f, OuterRadius, 0.0f);
-    DefaultHexVertices.Corner2 = FVector(InnerRadius, 0.5f * OuterRadius, 0.0f);
-    DefaultHexVertices.Corner3 = FVector(InnerRadius, -0.5f * OuterRadius, 0.0f);
-    DefaultHexVertices.Corner4 = FVector(0.0f, -OuterRadius, 0.0f);
-    DefaultHexVertices.Corner5 = FVector(-InnerRadius, -0.5f * OuterRadius, 0.0f);
-    DefaultHexVertices.Corner6 = FVector(-InnerRadius, 0.5f * OuterRadius, 0.0f);
-}
+    FVector(0.0f, UDMDHexMetrics::OuterRadius, 0.0f),
+    FVector(UDMDHexMetrics::InnerRadius, 0.5f * UDMDHexMetrics::OuterRadius, 0.0f),
+    FVector(UDMDHexMetrics::InnerRadius, -0.5f * UDMDHexMetrics::OuterRadius, 0.0f),
+    FVector(0.0f, -UDMDHexMetrics::OuterRadius, 0.0f),
+    FVector(-UDMDHexMetrics::InnerRadius, -0.5f * UDMDHexMetrics::OuterRadius, 0.0f),
+    FVector(-UDMDHexMetrics::InnerRadius, 0.5f * UDMDHexMetrics::OuterRadius, 0.0f),
+    FVector(0.0f, UDMDHexMetrics::OuterRadius, 0.0f)  // last vertice repeats the first one
+};
+
+const TArray<FVector2D> UDMDHexMetrics::HexMetrics2D =
+{
+    FVector2D(0.0f, UDMDHexMetrics::OuterRadius),
+    FVector2D(UDMDHexMetrics::InnerRadius, 0.5f * UDMDHexMetrics::OuterRadius),
+    FVector2D(UDMDHexMetrics::InnerRadius, -0.5f * UDMDHexMetrics::OuterRadius),
+    FVector2D(0.0f, -UDMDHexMetrics::OuterRadius),
+    FVector2D(-UDMDHexMetrics::InnerRadius, -0.5f * UDMDHexMetrics::OuterRadius),
+    FVector2D(-UDMDHexMetrics::InnerRadius, 0.5f * UDMDHexMetrics::OuterRadius),
+    FVector2D(0.0f, UDMDHexMetrics::OuterRadius)  // last vertice repeats the first one
+};
+
+UDMDHexMetrics::UDMDHexMetrics()
+{}
--- Source/DynamicMapDemo/Public/Mapper/DMDHexMetrics.h
+++ Source/DynamicMapDemo/Public/Mapper/DMDHexMetrics.h
@@ -6,35 +6,10 @@
 #include "UObject/NoExportTypes.h"
 #include "DMDHexMetrics.generated.h"
 
-/**
- * A Struct to hold vertices coordinates of 1U hex.
- */
-USTRUCT(BlueprintType)
-struct FHexVertices
-{
-	GENERATED_BODY()
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hex Corners")
-	FVector Corner1;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hex Corners")
-	FVector Corner2;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hex Corners")
-	FVector Corner3;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hex Corners")
-	FVector Corner4;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hex Corners")
-	FVector Corner5;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hex Corners")
-	FVector Corner6;
-};
 
 /**
- * Class that defines all metrics for game world.
+ * Class that defines all metrics for game world. 
+ * Most of them are static.
  */
 UCLASS(BlueprintType)
 class DYNAMICMAPDEMO_API UDMDHexMetrics : public UObject
@@ -44,17 +19,12 @@ class DYNAMICMAPDEMO_API UDMDHexMetrics : public UObject
 public:
 	UDMDHexMetrics();
 
-public:
-	// Outer radius for pointy side up hexagon
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Hex Metrics")
-	float OuterRadius = 10.0f;
-
-	// Inner radius for pointy side up hexagon
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Hex Metrics")
-	float InnerRadius = OuterRadius * 0.866025404f;
-	
-	// Default hex vertices coordinates (ratio for 1U hex)
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Hex Metrics")
-	FHexVertices DefaultHexVertices;
-
+	// Outer hex radius
+	static const float OuterRadius;
+	// Inner hex radius
+	static const float InnerRadius;
+	// Default coordinates of hex vertices in 3D
+	static const TArray<FVector> HexMetrics3D;
+	// Default coordinates of hex vertices in 2D
+	static const TArray<FVector2D> HexMetrics2D;
 };
