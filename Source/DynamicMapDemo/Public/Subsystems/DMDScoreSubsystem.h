1af8d187139945cd94a0f1e85ded0bc6e587ea1b
Станислав М <deafstats@live.ru>
2022-10-03 01:39:43 +0300

    Daily commit - score subsystem stub

---
+++ Source/DynamicMapDemo/Public/Subsystems/DMDScoreSubsystem.h
@@ -0,0 +1,17 @@
+// DynamicMap Demo. Copyright (C) 2022 Stanislav Moskalev
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "DMDScoreSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNAMICMAPDEMO_API UDMDScoreSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
